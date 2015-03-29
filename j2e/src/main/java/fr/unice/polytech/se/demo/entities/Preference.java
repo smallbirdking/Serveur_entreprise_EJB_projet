a3372c70880b90dc4bce696d62a7b27c7946f952
Feng DING <ding.feng@etu.unice.fr>
2015-03-29 23:47:51 +0200

    entity of preference

--- j2e/src/main/java/fr/unice/polytech/se/demo/entities/Preference.java
+++ j2e/src/main/java/fr/unice/polytech/se/demo/entities/Preference.java
@@ -1,7 +1,6 @@
 package fr.unice.polytech.se.demo.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 
 /**
@@ -12,4 +11,38 @@ import java.io.Serializable;
 @Table(name = "PREFERENCE")
 public class Preference implements Serializable{
 
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private Compte compte;
+
+    private InfoPayment infoPayment;
+
+    public Preference(){}
+
+    public Preference(Compte c, InfoPayment p){
+        this.compte = c;
+        this.infoPayment = p;
+    }
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "ID_COMPTE")
+    public Compte getCompte(){
+        return compte;
+    }
+
+    public void setCompte(Compte c){
+        compte = c;
+    }
+
+    @OneToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "ID_INFOPAYMENT")
+    public InfoPayment getInfoPayment(){
+        return infoPayment;
+    }
+
+    public void setInfoPayment(InfoPayment p){
+        infoPayment = p;
+    }
 }
