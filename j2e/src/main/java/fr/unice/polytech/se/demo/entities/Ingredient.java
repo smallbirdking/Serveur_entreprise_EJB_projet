f185fd5185bb65855c8f8e5abd7d021de82fd90c
Feng DING <ding.feng@etu.unice.fr>
2015-03-29 20:47:24 +0200

    entity of Ingredient

--- j2e/src/main/java/fr/unice/polytech/se/demo/entities/Ingredient.java
+++ j2e/src/main/java/fr/unice/polytech/se/demo/entities/Ingredient.java
@@ -1,24 +1,77 @@
 package fr.unice.polytech.se.demo.entities;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+import java.util.Set;
 
 /**
  * Created by ding on 22/03/15.
  */
+
 @Entity
 @Table(name = "INGREDIENT")
-public class Ingredient implements Serializable {
-    private String id_Ingredient;
+public class Ingredient implements Serializable{
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String nom_Ingredient;
+
+    private double prix_Ingredient;
+
+    private Set<Recette> recettes;
+
+    public Ingredient(){
+    }
+
+    public Ingredient(String n, double t){
+        this.nom_Ingredient = n;
+        this.prix_Ingredient = t;
+    }
+
+    public String toString(){
+        return "INGREDIENT[" + this.id + "]#" + this.nom_Ingredient + ", t: " + this.prix_Ingredient;
+    }
 
     @Id
-    public String getIdIngredient() {
-        return id_Ingredient;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_INGREDIENT",length = 10)
+    public Long getId() {
+        return id;
     }
 
-    public void setIdIngredient(String id_Ingredient) {
-        this.id_Ingredient = id_Ingredient;
+    @Column(name = "NOM_INGREDIENT", length = 32)
+    @NotNull
+    public String getNom_Ingredient() {
+        return nom_Ingredient;
     }
+
+    public void setNom_Ingredient(String n){
+        nom_Ingredient = n;
+    }
+
+    @Column(name = "PRIX_INGREDIENT", length = 10)
+    @NotNull
+    public double getPrix_Ingredient() {
+        return prix_Ingredient;
+    }
+
+    public void setPrix_Ingredient(double p){
+        prix_Ingredient = p;
+    }
+
+    @ManyToMany(cascade = {CascadeType.ALL, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @AssociationTable(table = @Table(name = "INGREDIENT_RECETTE"),
+            joinColumns = {@JoinColumn(name = "ID_RECETTE",referencedColumnName="ID_RECETTE")},
+            inverseJoinColumns = {@JoinColumn(name = "ID_INGREDIENT",referencedColumnName="ID_INGREDIENT")})
+    public Set<Recette> getRecettes(){
+        return recettes;
+    }
+
+    public void setRecettes(Set<Recette> r){
+        recettes = r;
+    }
+
+
 }
